1688377c39fc8099c0beb407669bea4fd24d274f
Ruchika <ruchikadeore163@gmail.com>
2024-11-04 16:56:46 +0530

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public ICollection<Book> Books { get; set; }
+
+    }
+}
---
+++ Models/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Member> Members { get; set; }
+    }
+}
