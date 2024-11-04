1cd2a32a547d7ffbea37cc74895cceacdbd97b10
NETHULA SUSHMA <sushmanethula732@gmail.com>
2024-11-04 17:33:55 +0530

    1stcommit

---
+++ Models/members.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement.Models
+{
+    public class members
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime MembershipDate { get; set; }
+    }
+}
