ad77f485aea58bb40413e85f69c68e4505a13118
jaiyashri <jaishrikrishanan@gmail.com>
2024-11-04 17:39:54 +0530

    1st commit

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public DateTime? PublishedDate { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string Author { get; set; }
+    }
+}
