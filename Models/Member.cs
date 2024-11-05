d140cdf596d22445768a30598928490ee945fff8
NETHULA SUSHMA <sushmanethula732@gmail.com>
2024-11-05 11:59:56 +0530

    Commited

---
+++ Models/Member.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagement.Models
+{
+    
+        public class Member
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public DateTime MembershipDate { get; set; }
+        }
+    
+}
